470070d30a2c94e4457a9f5b638096ccf643fe4e
nicomp42 <nicholdw@ucmail.uc.edu>
2025-04-16 10:41:13 -0400

    Add project files.

---
+++ PopulatingAControl/default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="PopulatingAControl._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+ <style type="text/css">
+        .ListBoxCssClass
+        {
+            color:GhostWhite;
+            background-color:DarkOliveGreen;
+            font-family:Courier New;
+            font-size:large;
+            font-style:italic;
+            }
+    </style>
+            <script type="text/javascript">
+            function handleListBoxDoubleClick(listBox) {
+                // Check if an item is actually selected
+                if (listBox.selectedIndex >= 0) {
+                    // Get the value of the selected item
+                    var selectedValue = listBox.options[listBox.selectedIndex].value;
+                    // Trigger a postback using __doPostBack
+                    // The first argument is the UniqueID of the control causing the postback (can be the listbox itself)
+                    // The second argument is a string containing event information (e.g., "DoubleClick:" + value)
+                    __doPostBack(lbNames.id, 'DoubleClick:' + selectedValue);
+                }
+            }
+            </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ondblclick="handleListBoxDoubleClick(this);" 
+                CssClass="ListBoxCssClass" ID="lbNames" runat="server" OnSelectedIndexChanged="lbNames_SelectedIndexChanged">
+                <asp:ListItem>Avocado</asp:ListItem>
+                <asp:ListItem>Peach</asp:ListItem>
+                <asp:ListItem>Plum</asp:ListItem>
+                <asp:ListItem>Orange</asp:ListItem>
+                <asp:ListItem>Guava</asp:ListItem>
+            </asp:ListBox>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br/>
+            <asp:TextBox ID="txtNewItem" runat="server"></asp:TextBox>
+            <asp:Button ID="cmdAdd" runat="server" Text="Button" OnClick="cmdAdd_Click" />
+        </div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+    </form>
+</body>
+</html>
---
+++ PopulatingAControl/default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PopulatingAControl
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Check if the page request is a postback
+            if (IsPostBack)
+            {
+                // Retrieve the control ID that caused the postback
+                // __EVENTTARGET stores the UniqueID of the control passed to __doPostBack
+                string eventTarget = Request.Form["__EVENTTARGET"];
+
+                // Retrieve the argument passed to __doPostBack
+                // __EVENTARGUMENT stores the second argument passed to __doPostBack
+                string eventArgument = Request.Form["__EVENTARGUMENT"];
+
+                // Check if the postback was triggered by our ListBox double-click handler
+                if (eventTarget == lbNames.ID && eventArgument != null && eventArgument.StartsWith("DoubleClick:"))
+                {
+                    // Extract the value passed in the argument
+                    string doubleClickedValue = eventArgument.Substring("DoubleClick:".Length);
+
+                    // Find the corresponding item text (optional, if needed)
+                    System.Web.UI.WebControls.ListItem item = lbNames.Items.FindByValue(doubleClickedValue);
+                    string doubleClickedText = (item != null) ? item.Text : "Unknown Item";
+
+                    // --- Your Server-Side Logic Here ---
+                    // Execute the code you want to run on double-click
+                    Label1.Text = lbNames.Text;
+
+                    // Example: Redirect, update database, perform calculation, etc.
+                    // Response.Redirect($"DetailsPage.aspx?item={doubleClickedValue}");
+                }
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // click event handler for the button
+            // Copy the selected item from the
+            //  list box into the label
+            Label1.Text = lbNames.Text;
+        }
+
+        protected void cmdAdd_Click(object sender, EventArgs e)
+        {
+            // Runs when the user clicks cmdAdd
+            lbNames.Items.Add(txtNewItem.Text);
+        }
+
+        protected void lbNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
